1e57688654852ccf33f70667f590c68578ce11c7
nyx <sakusennx@gmail.com>
2021-05-08 06:12:49 +0900

    first commit.

---
+++ src/main/java/com/example/herokugetstartjava/HerokuGetStartJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.herokugetstartjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuGetStartJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerokuGetStartJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/herokugetstartjava/HerokuGetStartJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.herokugetstartjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuGetStartJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
